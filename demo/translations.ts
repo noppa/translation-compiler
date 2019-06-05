b3731751b3506d72a880247679d2a14d96455e27
noppa <the.nobba@gmail.com>
2019-06-05 20:10:38 +0300

    [WIP] Traversing for translate keys

--- demo/translations.ts
+++ demo/translations.ts
@@ -3,13 +3,12 @@ import { t } from '../src/runtime'
 export default {
 	a: {
 		b: {
-			foo: t((a: { x: number }) => {
+			woop: t((a: { x: number }) => {
 				return {
 					fi: 'Foo fi' + a.x,
 					en: 'Foo en',
 				}
 			}),
-			...null,
 		},
 	},
 }
